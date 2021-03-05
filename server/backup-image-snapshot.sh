710c224f8025990146b522650b5973cb6f898173
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2021-03-05 11:44:32 +0100

    cleanup old backups

--- server/backup-image-snapshot.sh
+++ server/backup-image-snapshot.sh
@@ -7,6 +7,10 @@ NOW=$(date +"%Y%m%d%H%M")
 BACKUP_DIR="backups/image_snapshots"
 mkdir -p $BACKUP_DIR
 
+# tar the Pharo files
 tar -czf $BACKUP_DIR/mediaclue_image_$NOW.tgz pharo/mediaclue.image pharo/mediaclue.changes
+
+# Delete old
+find $BACKUP_DIR -iname "*.tgz" -not -newermt "100 days ago" -delete
 
 popd > /dev/null
