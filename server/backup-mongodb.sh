8f2eb4b93411e742286da32bd971bfb0799ac84b
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2022-12-02 09:32:19 +0100

    minor

--- server/backup-mongodb.sh
+++ server/backup-mongodb.sh
@@ -10,4 +10,7 @@ mkdir -p $BACKUP_DIR
 
 mongodump --db=mediaclue --gzip --out=$BACKUP_DIR
 
+# Delete old
+find backups/mongodb/ -iname "mongodb_*" -type d -not -newermt "100 days ago" -exec rm -dr "{}" \;
+
 popd > /dev/null
