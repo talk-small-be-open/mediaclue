60c52ff8c6119b002fe23aac9d0b32d2156e961b
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2025-06-18 18:14:58 +0200

    minor dev

--- deployment/install.sh
+++ deployment/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # add --ask-become-pass if sudo needs password
-ansible-playbook -i inventory_$1.yml -e customerCode=$1  playbook_install.yml
+ansible-playbook --ask-become-pass -i inventory_$1.yml -e customerCode=$1  playbook_install.yml
