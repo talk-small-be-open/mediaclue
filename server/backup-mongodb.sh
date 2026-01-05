10e3e83faff568e522ca9b7e913f9102cf237910
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2026-01-05 12:07:50 +0100

    minor fixes in maintenance scripts

--- server/backup-mongodb.sh
+++ server/backup-mongodb.sh
@@ -13,6 +13,6 @@ mkdir -p $BACKUP_DIR
 mongodump --db=mediaclue --gzip --out=$BACKUP_DIR
 
 # Delete old
-find backups/mongodb/ -iname "mongodb_*" -type d -not -newermt "100 days ago" -exec rm -dr "{}" \;
+find backups/mongodb/ -depth -iname "mongodb_*" -type d -not -newermt "100 days ago" -exec rm -dr "{}" \;
 
 popd > /dev/null
