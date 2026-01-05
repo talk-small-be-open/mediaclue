18e1be64758a348706742056fcb6165d6e3f2db5
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2026-01-05 12:36:32 +0100

    mongodb dump quiet for cron

--- server/backup-mongodb.sh
+++ server/backup-mongodb.sh
@@ -10,7 +10,7 @@ BACKUP_DIR="backups/mongodb/mongodb_$NOW"
 
 mkdir -p $BACKUP_DIR
 
-mongodump --db=mediaclue --gzip --out=$BACKUP_DIR
+mongodump --quiet --db=mediaclue --gzip --out=$BACKUP_DIR
 
 # Delete old
 find backups/mongodb/ -depth -iname "mongodb_*" -type d -not -newermt "100 days ago" -exec rm -dr "{}" \;
